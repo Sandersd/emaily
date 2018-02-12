1b94dd400bfae2dc1afdf0cd81d8f254bcd4d733
Sandersd <sandersd27@gmail.com>
2018-02-11 23:25:27 -0800

    authorization

---
+++ .js/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '178314497001-s198cjf37thco8phnqspkd60jaihdgur.apps.googleusercontent.com',
+  googleClientSecret: 'PORkojxLbnz5vRhjEb65GAyZ',
+  mongoURI: 'mongodb://dylan:password@ds231658.mlab.com:31658/dms-emaily-dev',
+  cookieKey: 'sdadasdsadasdffffrrr'
+};
